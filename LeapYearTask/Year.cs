643e6b94a63251e64a4425fc6474011da7ded91a
a91n0d <a91n0d@gmail.com>
2022-01-12 09:25:23 +0300

    Add project files.

---
+++ LeapYearTask/Year.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LeapYearTask
+{
+    public static class Year
+    {
+        /// <summary>
+        /// Report if given year is a leap year.
+        /// </summary>
+        /// <param name="year">Given year.</param>
+        /// <returns>True if given year is leap, false otherwise.</returns>
+        public static bool IsLeapYear(int year)
+        {
+            return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+        }
+    }
+}
